067c26b6a9e5ad8b4b3ee297363655d555f2dca5
cheomji <104716784+cheomji@users.noreply.github.com>
2023-03-29 16:55:34 +0900

    Create PolarPoint.java

---
+++ 230329/PolarPoint.java
@@ -0,0 +1,37 @@
+
+public class PolarPoint extends Point {
+	private double r;
+	private double theta;
+	
+	public PolarPoint (double r, double theta) {
+		super(r*Math.cos(theta*Math.PI/180), r*Math.sin(theta*Math.PI/180));
+		this.r = r; this.theta = theta;
+	}
+	
+	public double getR() {
+		return this.r;
+	}
+	public double getTheta() {
+		return this.theta;
+	}
+	
+	@Override
+	public double distFromOrigin() {
+		return this.r;
+	}
+	
+	@Override
+	public void setX(double x) {
+		super.setX(x);
+		this.r = super.distFromOrigin();
+		theta = Math.atan2(this.getY(), this.getX())*180/Math.PI;
+	}
+	
+	@Override
+	public void setY(double y) {
+		super.setY(y);
+		this.r = super.distFromOrigin();
+		theta = Math.atan2(this.getY(), this.getX())*180/Math.PI;
+		
+	}
+}
